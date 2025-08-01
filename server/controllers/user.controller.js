dc61923f4ce15ba50c8a70ddfe9a39370c619211
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-08-01 23:10:34 +0545

    registration and email verification process completed

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,7 +5,7 @@ import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 
 export async function registerUserController(request, response) {
   try {
-    const { name, email, password } = request.body 
+    const { name, email, password } = request.body;
 
     if (!name || !email || !password) {
       return response.status(400).json({
@@ -62,3 +62,40 @@ export async function registerUserController(request, response) {
     });
   }
 }
+
+export async function verifyEmailController(request, response) {
+  try {
+    const { code } = request.body;
+
+    const user = await UserModel.findOne({ _id: code });
+
+    if (!user) {
+      return response.status(400).json({
+        message: "Invalid code",
+        error: true,
+        success: false,
+      });
+    }
+
+    const updateUser = await UserModel.updateOne(
+      { _id: code },
+      {
+        verify_email: true,
+      }
+    );
+
+    return response.json({
+      message: "Verify email done",
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: true,
+    });
+  }
+}
+
+//login controller
