9d458d2d95719b8a05a1a2216dbc178580760a55
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-08-02 13:57:54 +0545

    email sent to the registered user successfully

--- server/utils/verifyEmailTemplate.js
+++ server/utils/verifyEmailTemplate.js
@@ -2,7 +2,7 @@ const verifyEmailTemplate = ({ name, url }) => {
   return `
 <p>Dear ${name}</p>    
 <p>Thank you for registering Foodies.</p>   
-<a href=${url} style="color:black;background:orange;margin-top:10px;padding:20px;display:block">
+<a href=${url} style="color:black; background:orange; margin-top:10px; padding:20px; display:block; text-decoration:none; font-weight:bold; width:fit-content; border-radius:5px;">
     Verify Email
 </a>
 `;
