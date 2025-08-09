289d2eaa3b26525066af84151a5dc91755bf1a6a
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-08-09 09:21:46 +0545

    update

---
+++ client/src/components/UploadSubCategoryModel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UploadSubCategoryModel = () => {
+  return (
+    <section className='fixed top-0 right-0 bottom-0 left-0 bg-neutral-800/70 z-50 '>
+      <div>
+        
+      </div>
+
+    </section>
+  )
+}
+
+export default UploadSubCategoryModel
--- client/src/pages/SubCategoryPage.jsx
+++ client/src/pages/SubCategoryPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import UploadSubCategoryModel from "../components/UploadSubCategoryModel";
 
 const SubCategoryPage = () => {
+  const [openAddSubCategory, setOpenAddSubCategory] = useState(false);
   return (
-    <div>
-      <h1>Sub Category Page</h1>
-    </div>
-  )
-}
+    <section>
+      <div className="p-2  bg-white shadow-md rounded-lg mb-4 flex items-center justify-between">
+        <h2 className="font-semibold">Sub Category</h2>
+        <button
+          onClick={() => setOpenAddSubCategory(true)}
+          className="mt-2 px-3 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer text-sm"
+        >
+          Add Sub Category
+        </button>
+      </div>
 
-export default SubCategoryPage
+      {openAddSubCategory && (
+        <UploadSubCategoryModel onClose={() => setOpenAddSubCategory(false)} />
+      )}
+    </section>
+  );
+};
+
+export default SubCategoryPage;
